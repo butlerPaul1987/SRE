f54e79ef347321a2c41e95de980fc5bb5e89ab94
Paul Butler <57191996+butlerPaul1987@users.noreply.github.com>
2025-11-15 11:50:22 +0000

    Create exceptions.py

---
+++ Python/webscraper/MorrisonsPriceCheck/common/exceptions.py
@@ -0,0 +1,6 @@
+# Start custom classes for exceptions
+class TokenFailed(Exception): pass
+class APIParseFailed(Exception): pass
+class ProductNotFound(Exception): pass
+class RetryLimitExceeded(Exception): pass
+# End custom classes for exceptions
